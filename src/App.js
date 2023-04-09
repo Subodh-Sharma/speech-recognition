6d9f0882f5f08bfe4784d86def5db3882d856260
Subodh Sharma <sharma2000subodh@gmail.com>
2023-04-10 00:00:10 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import useClipboard from "react-use-clipboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [textToCopy, setTextToCopy] = useState();
+    const [isCopied, setCopied] = useClipboard(textToCopy, {
+        successDuration:1000
+    });
+
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const setEmpty = () => {
+      window.location.reload();
+    }
+    if (!browserSupportsSpeechRecognition) {
+        return null
+    }
+    
+
+    return (
+        <>
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br/>
+                <p>A React hook that converts speech from the microphone to text and makes it available to your React
+                    components.</p>
+
+                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                    {transcript}
+                </div>
+
+                <div className="btn-style">
+
+                    <button onClick={setCopied}>
+                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
+                    </button>
+                    <button onClick={setEmpty}>
+                        Empty clipboard
+                    </button>
+                    <button className="start-btn" onClick={startListening}>Start Listening</button>
+                    <button className="stop-btn" onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default App;
+
